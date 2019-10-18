bf89b3d6e8f14c1ac593fb9d7de1be2123f3fc6f
Ubuntu <azureuser@spark-vm.jgn1u5syjwiexowukxycxncrxf.ix.internal.cloudapp.net>
2019-10-18 09:37:40 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+
+namespace SentinmentAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession
+                .Builder()
+                .AppName("Sentiment Analysis with Spark")
+                .GetOrCreate();
+
+            // Create initial DataFrame
+            DataFrame df = spark
+                .Read()
+                .Option("header", true)
+                .Option("inferSchema", true)
+                .Csv("tweets_clean_data.csv");
+
+            df.Show();
+
+            df.CreateOrReplaceTempView("TweetsTextView");
+
+            spark.Stop();
+
+            // Count words
+        }
+    }
+}
